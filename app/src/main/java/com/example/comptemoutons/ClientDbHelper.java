7c08941987e2f67fff89456aa0f43d7deafd84fa
Jul le S <cl.jankowiak@gmail.com>
2019-03-04 16:59:57 +0100

    premiere iteration

---
+++ app/src/main/java/com/example/comptemoutons/ClientDbHelper.java
@@ -0,0 +1,29 @@
+package com.example.comptemoutons;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ClientDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "mouton.db";
+
+    public final String SQL_CREATE = "CREATE TABLE User (id INTEGER PRIMARY KEY, pwd TEXT);";
+    public final String SQL_DELETE = "DROP TABLE IF EXISTS Clients;";
+
+    public ClientDbHelper(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE);
+        onCreate(db);
+    }
+}
