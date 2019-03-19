422cda2867ea33c47b84c14a3b8795213b173b01
Jul le S <cl.jankowiak@gmail.com>
2019-03-19 10:36:25 +0100

    commit final

--- app/src/main/java/com/example/comptemoutons/About.java
+++ app/src/main/java/com/example/comptemoutons/About.java
@@ -1,7 +1,11 @@
 package com.example.comptemoutons;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class About extends AppCompatActivity {
 
@@ -13,6 +17,10 @@ public class About extends AppCompatActivity {
         // Bouton de retour sur la barre de menu
         assert getSupportActionBar() != null; // éviter le nullpointerexception
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setLink(R.id.imageViewLacroux,"https://fr.linkedin.com/in/nathan-lacroux");
+        setLink(R.id.imageViewBedex,"https://fr.linkedin.com/in/antoine-b%C3%A9dex-a09451170");
+
     }
 
     @Override
@@ -20,5 +28,18 @@ public class About extends AppCompatActivity {
         // retour à l'activité précédente
         finish();
         return super.onSupportNavigateUp();
+    }
+
+    private void setLink(int id, final String url) {
+        ImageView image = (ImageView) findViewById(id);
+        image.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
